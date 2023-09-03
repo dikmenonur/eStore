30a50803599c4c27154d086f8917d72b1284fb91
Onur Dikmen <onur.dikmen@rise-consulting.net>
2023-09-03 17:50:26 +0300

    ocelot edit rooting

--- apigateways/eStore.Ocelot.API/eStore.Ocelot.API/Program.cs
+++ apigateways/eStore.Ocelot.API/eStore.Ocelot.API/Program.cs
@@ -1,27 +1,70 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+        builder.Services.AddOcelot(builder.Configuration);
+
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", buider =>
+            {
+                buider.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+
+
+
+        builder.Services.AddSwaggerForOcelot(builder.Configuration, x =>
+        {
+            x.GenerateDocsForGatewayItSelf = false;
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseCors("CorsPolicy");
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+
+        app.UseRouting();
+
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", context =>
+            {
+                context.Response.Redirect("swagger/index.html");
+                return Task.CompletedTask;
+            });
+        });
+
+
+        app.UseSwaggerForOcelotUI(
+            opt => { opt.PathToSwaggerGenerator = "/swagger/docs"; });
+
+        await app.UseOcelot();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
